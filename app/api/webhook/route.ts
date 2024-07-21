cb60c9efa4c9dbb65844420fbedeb685e382cfe5
areyaar <saket.tanki@gmail.com>
2024-07-21 12:28:42 +0530

    added webhook

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,50 @@
+import { getVideoWithNotes } from '@/app/lib/actions/getVideoWithNotes';
+import prisma from "@/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { updateVideo } from "@/app/lib/actions/updateVideo";
+
+
+export async function GET() {
+    
+    return NextResponse.json({
+        message: "Hello there"
+    }, {
+        status: 200
+    })
+}
+
+
+export async function POST(request:NextRequest) {
+    try {
+        const body = await request.json();
+        const { id, text } = body;
+
+        if (!id || !text) {
+            return NextResponse.json({
+                message: "Missing required fields",
+            }, {
+                status: 400 // Bad Request
+            });
+        }
+        // first I should save the text
+        const updateResponse = await updateVideo(id, text); // update hua 
+        const note = await getVideoWithNotes(id);
+        //console.log(id, text);
+        return NextResponse.json({
+            message: "Data received successfully",
+            receivedData: { id, text }
+        }, {
+            status: 201 // Created
+        });
+
+    } catch (error) {
+        // TODO: update failed status 
+
+        return NextResponse.json({
+            message: "Error processing request",
+            error: error
+        }, {
+            status: 400 // Bad Request
+        });
+    }
+}
--- app/lib/actions/sendToConvert.ts
+++ app/lib/actions/sendToConvert.ts
@@ -16,10 +16,9 @@ export async function sendToConvert(url: string) {
                 'Content-Type': 'application/json',
             },
         })
-        const updateResponse = await updateVideo(newVidId, response.data.text)
-        const generateNotes = await getVideoWithNotes(newVidId);
+        
         revalidatePath('/convert')
-        // yahi pe prisma se update kr skte kuki this is running on the server
+        
         return response.data
     } catch (error) {
         console.error('Error in Converting:', error)
