65f52e548fa70f81daa802790e8deaff661e3b31
areyaar <saket.tanki@gmail.com>
2024-07-21 15:38:39 +0530

    tried to handle unwanted caching

--- components/AllVideoCards.tsx
+++ components/AllVideoCards.tsx
@@ -1,6 +1,8 @@
+import { revalidatePath } from "next/cache";
 import VideoCard from "./VideoCard"
 import { getAllVideos } from "@/app/lib/actions/getAllVideos"
 export default async function AllVideoCards() {
+    revalidatePath("/convert"); // maybe will stop catching
     const videos = await getAllVideos();
     return (
         <div>
